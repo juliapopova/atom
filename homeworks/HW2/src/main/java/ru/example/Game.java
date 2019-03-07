e0378190d3e7429b0b34f1890889ec72362cf36b
juliapopova <44148548+juliapopova@users.noreply.github.com>
2019-03-07 21:50:20 +0300

    нахождение слова перенесено в отдельный метод. Добавлен метод быки

--- homeworks/HW2/src/main/java/ru/example/Game.java
+++ homeworks/HW2/src/main/java/ru/example/Game.java
@@ -5,26 +5,38 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.Scanner;
 
 public class Game {
 
     public static void main (String[] argc)
     {
-        System.out.println("Welcome to Bulls and game!");
-        List<String> dictionary = new ArrayList<String>();
+        System.out.println("Welcome to Bulls and Cows game!");
+        String wordGame = newWord();
+        Scanner in = new Scanner(System.in);
+        System.out.println(wordGame);
+        System.out.println("The word have " + wordGame.length() + " letters");
+        System.out.println("Good luck!");
+    }
 
+    public static String newWord ()
+    {
+
+        ArrayList<String> dictionary = new ArrayList<String>();
+        String line;
 
         try {
-            File file = new File("C:\\Users\\а\\Documents\\GitHub\\atom\\homeworks\\HW2\\dictionary");
+            File file = new File("dictionary.txt");
             //создаем объект FileReader для объекта File
             FileReader fr = new FileReader(file);
             //создаем BufferedReader с существующего FileReader для построчного считывания
             BufferedReader reader = new BufferedReader(fr);
             // считаем сначала первую строку
-            dictionary.add(0) = reader.readLine();
+            line = reader.readLine();
             while (line != null) {
-                System.out.println(line);
-                // считываем остальные строки в цикле
+                dictionary.add(line);
                 line = reader.readLine();
             }
         } catch (FileNotFoundException e) {
@@ -32,5 +44,17 @@ public class Game {
         } catch (IOException e) {
             e.printStackTrace();
         }
+        int randIndex = ThreadLocalRandom.current().nextInt(0, dictionary.size());
+        return dictionary.get(randIndex);
+    }
+
+    public static int countBulls (String wordPlayer, String wordGame)
+    {
+        int count = 0;
+        for (int i = 0; i < wordGame.length(); i++)
+            if (wordGame.charAt(i) == wordPlayer.charAt(i))
+                count++;
+
+        return count;
     }
 }
